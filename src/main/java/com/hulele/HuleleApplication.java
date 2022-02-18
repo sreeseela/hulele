27a36139e5f035fba2a6958309f2828f65c4358f
manjunath3750 <manjunathgudibanda228@gmail.com>
2022-02-18 17:21:12 +0530

    Added annotations

--- src/main/java/com/hulele/HuleleApplication.java
+++ src/main/java/com/hulele/HuleleApplication.java
@@ -1,4 +1,4 @@
-package com.configserver;
+package com.hulele;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/hulele/model/Buyer.java
+++ src/main/java/com/hulele/model/Buyer.java
@@ -2,10 +2,21 @@ package com.hulele.model;
 
 import java.util.Date;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Buyer")
 public class Buyer {
+	@Id
 	private int id;
 	private String name;
-	private String sessId;
 	private int credId;
 	private int countryCode;
 	private Date dob;
