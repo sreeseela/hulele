05efb62ce5d61a2bbbc4de7b3f046f51550ce336
manjunath3750 <manjunathgudibanda228@gmail.com>
2022-02-18 19:08:12 +0530

    added a controller

--- src/main/java/com/hulele/controller/HomeController.java
+++ src/main/java/com/hulele/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.hulele.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
-	
+	@GetMapping("")
+	public String home() {
+		return "Hello World";
+	}
 }
