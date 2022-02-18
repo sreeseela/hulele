0fa3422cf410e76dab2cefbf80fbd7a6030c28dd
manjunath3750 <manjunathgudibanda228@gmail.com>
2022-02-18 18:02:57 +0530

    added category and credential model

--- src/main/java/com/hulele/model/Seller.java
+++ src/main/java/com/hulele/model/Seller.java
@@ -3,11 +3,18 @@ package com.hulele.model;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "Seller")
 public class Seller {
 	@Id
 	private int id;
-	private int name;
-	private int address;
-	private int zipcode;
+	private String name;
+	private String address;
+	private String zipcode;
 }
