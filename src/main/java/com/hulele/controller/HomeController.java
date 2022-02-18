72fb2b9c8ac1f81a2735f0a8fdde854fd0462558
unknown <ganeshdatha777@gmail.com>
2022-02-18 18:49:49 +0530

    new file:   src/main/java/com/hulele/controller/HomeController.java 	modified:   src/main/java/com/hulele/repository/CategoryRep.java 	modified:   src/main/java/com/hulele/repository/CredentialsRep.java 	modified:   src/main/java/com/hulele/repository/OrderRep.java

---
+++ src/main/java/com/hulele/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.hulele.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+}
--- src/main/java/com/hulele/repository/CredentialsRep.java
+++ src/main/java/com/hulele/repository/CredentialsRep.java
@@ -1,9 +1,11 @@
 package com.hulele.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hulele.model.Credentials;
 
+@Repository
 public interface CredentialsRep extends JpaRepository<Credentials, Integer>{
 
 }
