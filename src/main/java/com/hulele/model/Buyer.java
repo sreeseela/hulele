7f5f1bd695c86d333bacfd4398d455dc0b57f13b
unknown <ganeshdatha777@gmail.com>
2022-02-18 17:03:30 +0530

    new file:   src/main/java/com/hulele/model/Buyer.java

---
+++ src/main/java/com/hulele/model/Buyer.java
@@ -0,0 +1,12 @@
+package com.hulele.model;
+
+import java.util.Date;
+
+public class Buyer {
+	private int id;
+	private String name;
+	private String sessId;
+	private int credId;
+	private int countryCode;
+	private Date dob;
+}
